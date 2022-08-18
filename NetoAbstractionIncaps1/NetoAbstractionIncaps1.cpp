901954808be22f467e9e5f8204152667384fa61a
DenisMir <pete4kob12@mail.ru>
2022-08-19 00:21:10 +0300

    work2

--- NetoAbstractionIncaps1/NetoAbstractionIncaps1.cpp
+++ NetoAbstractionIncaps1/NetoAbstractionIncaps1.cpp
@@ -10,66 +10,87 @@ private:
     std::string Street;
     int home;
     int room;
-
 public:
     Address()
     {
-    Country = "";
-    Street = "";
-    home = 0;
-    room = 0;
+        Country = "";
+        Street = "";
+        home = 0;
+        room = 0;
+    }
+    Address(std::string Country, std::string Street, int home, int room)
+    {
+    this->Country = Country;
+    this->Street = Street;
+    this->home = home;
+    this->room = room;
     }
     void set_address(std::string C, std::string S, int h, int r)
     {
-        this->Country = C;
-        this->Street = S;
-        this->home = h;
-        this->room = r;
+        Country = C;
+        Street = S;
+        home = h;
+        room = r;
     }
     std::string get_output_address()
     {
-        return Country + "\n" + Street + "\n" + std::to_string(home) + "\n" + std::to_string(room);
+        return Country + "," + Street + "," + std::to_string(home) + "," + std::to_string(room);
     }
 };
 
-void out_file(Address &Ad)
+int out_file(Address *arrADR)
 {
     std::cout << "Файл успешно открыт!!!" << std::endl;
-    Address adr;
-    std::ifstream file("in.txt");
-    std::string C;
-    std::string St;
-    int h;
-    int r;
-    int count;
-    if (file.is_open())
+    std::ifstream filer("in.txt");
+    std::string Country;
+    std::string Street;
+    int home;
+    int room;
+    int countAddress;
+
+    if (filer.is_open())
     {
-        file >> count;
-        std::cout << count << std::endl;
-        for (int i = 0; i < count; i++)
+
+        filer >> countAddress;
+        std::cout << countAddress << std::endl;
+        int iter = 0;
+        for (int i = 0; i < countAddress; i++)
         {
-            file >> C;
-            file >> St;
-            file >> h;
-            file >> r;
-            Ad.set_address(C, St, h, r);
-            std::cout << adr.get_output_address();
+            filer >> Country;
+            filer >> Street;
+            filer >> home;
+            filer >> room;
+            arrADR[iter] = Address(Country, Street, home, room);
+            iter++;
         }
+        std::cout << arrADR[2].get_output_address() + "\n";
+        std::cout << arrADR[1].get_output_address() + "\n";
+        std::cout << arrADR[0].get_output_address() + "\n";
     }
     else
     {
         std::cout << "Не получилось открыть файл!" << std::endl;
     }
-    file.close();
+    return countAddress;
+    filer.close();
 }
 
+void writeFile(Address *arrADR, int countadr)
+{
+    std::ofstream filew("out.txt");
+    filew << countadr << "\n";
+    filew << arrADR[2].get_output_address() + "\n";
+    filew << arrADR[1].get_output_address() + "\n";
+    filew << arrADR[0].get_output_address() + "\n";
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    //SetConsoleOutputCP(1251);
-    //SetConsoleCP(1251);
-    Address adr;
-    out_file(adr);
 
+    Address* arrADR = new Address[5];
+    int countadr = out_file(arrADR);
+
+    writeFile(arrADR,countadr);
+    delete [] arrADR;
 }
